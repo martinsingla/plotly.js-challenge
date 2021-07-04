fbaf33b2befd82c194d7b3c426df34cab24baf71
Martín Singla <martinsingla@MacBook-Pro-de-Martin.local>
2021-07-04 00:58:28 -0300

    updates

--- app.js
+++ app.js
@@ -1,18 +1,59 @@
-//Defining functions
-var chosenID = 0
-var chosenDemographics = 0
-var sampleData = 0
+////////////////////////////////////////////////////////////
 
-//Loading JSON to explore data structure
+//Defining functions & variables
+
+//Chosen ID value function
+function findChosenID(){
+    return d3.select("#selDataset").property("value");
+}
+
+//Create Demographics Data Table Function
+function updateDemographics(data){
+    //find data for selected ID
+    data.metadata.forEach(i => {
+        if(parseInt(i.id) === parseInt(findChosenID())){
+            var chosenDemographics = i;
+            //Assign data to Demographics table
+            var DemographicData = d3.select("#sample-metadata");
+            DemographicData.html("") //empy table
+            Object.entries(chosenDemographics).forEach(([key, value]) => {
+                DemographicData.append('p')
+                .text(`${key.toUpperCase()}: ${value}`)
+            });
+        }
+    });
+}
+
+//Create Top 10 found bacteria Chart
+function updateTop10Chart(data){
+    //find data for chosen ID sample
+    data.samples.forEach(i => {
+        if(parseInt(i.id) === parseInt(findChosenID())){
+            var sampleData = i;
+            console.log(sampleData)
+        }
+    })
+}
+
+
+//Event handler function (update all tables/charts on selected ID change)
+function changeHandler(){
+    d3.json('./data/samples.json').then(function(importData){
+        var data = importData;
+        updateDemographics(data);
+        updateTop10Chart(data);
+    })
+}
+
+////////////////////////////////////////////////////////////
+//initiate App
+
 d3.json('./data/samples.json').then(function(importData){
-
-    //store data
     var data = importData;
     console.log(data);
 
     //get subject ids and assign them to selector
     var subjectIds = data.metadata.map(i => i.id);
-
     IdSelector = d3.select("#selDataset");
     subjectIds.forEach(id => {
         IdSelector.append('option')
@@ -20,42 +61,13 @@ d3.json('./data/samples.json').then(function(importData){
         .attr('value', id);
     });
 
-    //selected ID
-    chosenID = d3.select("#selDataset").property("value");
-    console.log(chosenID);
-
-    //Demographic Data
-    data.metadata.forEach(i => {
-
-        //find sample id
-        if(parseInt(i.id) === parseInt(chosenID)){
-            chosenDemographics = i;
-            console.log(chosenDemographics);
-
-            //Assign info to demographics panel
-            var DemographicData = d3.select("#sample-metadata");
-
-            Object.entries(chosenDemographics).forEach(([key, value]) => {
-                console.log(`${key.toUpperCase()}: ${value}`)
-
-                DemographicData.append('p')
-                .text(`${key.toUpperCase()}: ${value}`)
-            });
-        }
-    });
-
-    //Create charts
-    data.samples.forEach(i => {
-
-        //find sample ID
-        if(parseInt(i.id) === parseInt(chosenID)){
-
-            sampleData = i 
-            console.log(sampleData)
-
-        }
-
-    })
-
+    //Assign table & graphs
+    updateDemographics(data);
+    updateTop10Chart(data);
 
 });
+
+////////////////////////////////////////////////////////////
+//Observe changes
+
+d3.selectAll('#selDataset').on('change', changeHandler);
